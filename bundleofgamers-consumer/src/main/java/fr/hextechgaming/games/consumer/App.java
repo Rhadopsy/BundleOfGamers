95349f4df0198fce38d217fb629e1eebf625324d
Rhadopsy <f.etendard@gmail.com>
2021-01-29 17:22:56 +0100

    premier commit

---
+++ bundleofgamers-consumer/src/main/java/fr/hextechgaming/games/consumer/App.java
@@ -0,0 +1,13 @@
+package fr.hextechgaming.games.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
